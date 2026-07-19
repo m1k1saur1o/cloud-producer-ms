8a8e3c48ae5c6f0e8a5e8767105aaa5d834a49cf
Miguel G <miguel.gar.ven@gmail.com>
2026-07-19 03:12:25 -0400

    Remove commented test line
    Removed commented test line from ProducerMsApplication.

--- src/main/java/com/example/producer/ProducerMsApplication.java
+++ src/main/java/com/example/producer/ProducerMsApplication.java
@@ -3,7 +3,7 @@ package com.example.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//test
+
 @SpringBootApplication
 public class ProducerMsApplication {
 
